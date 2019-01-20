dc9168cc3bd02d118d5b694ab35d22c46ae9e814
jhyang12345 <jhyang12345@naver.com>
2019-01-20 20:40:23 +0900

    Implemented constrolled component on radio buttons

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,10 +6,31 @@ import avatar from '../images/avatar.png'
 
 class Question extends Component {
 
+  state = {
+    selected: null,
+    optionOne: false,
+    optionTwo: false,
+  }
+
+  handleOptionChange = (evt) => {
+    const name = evt.target.name
+    const value = evt.target.value
+    const checked = evt.target.checked
+
+
+    this.setState(() => ({
+      selected: value,
+      [value]: checked,
+    }))
+    console.log(this.state)
+    console.log(value)
+  }
+
   render() {
     const { question } = this.props
     // author is id of author not the name
     const { author, id, optionOne, optionTwo, timestamp } = question
+
     console.log(optionOne);
     return (
       <div className="question-container">
@@ -20,11 +41,23 @@ class Question extends Component {
         <div className="options-holder">
           <form className="options-form">
             <div className="radio-container">
-              <input name="radio_{id}" type="radio" value="optionOne"/>
+              <input
+                onChange={this.handleOptionChange}
+                name={"radio_" + id}
+                type="radio"
+                value="optionOne"
+                checked={this.state.optionOne}
+                />
               <label htmlFor="optionOne">{optionOne.text}</label>
             </div>
             <div className="radio-container">
-              <input name="radio_{id}" type="radio" value="optionTwo"/>
+              <input
+                onChange={this.handleOptionChange}
+                name={"radio_" + id}
+                type="radio"
+                value="optionTwo"
+                checked={this.state.optionTwo}
+                />
               <label htmlFor="optionTwo">{optionTwo.text}</label>
             </div>
             <div>
