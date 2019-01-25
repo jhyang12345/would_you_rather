764d4b737759c2f871ba4072e17b4939a5a0385f
Jae-Hyeong <jhyang12345@naver.com>
2019-01-25 16:07:45 +0900

    Handled Questions filtering logic

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,11 +4,34 @@ import Question from './Question'
 import "../css/Dashboard.css"
 
 class Dashboard extends Component {
+  state = {
+    // if answered is set to true render questions that have been answered
+    answered: false,
+  }
+
+  hasVoted = (question) => {
+    const { authorizeUser } = this.props
+    const id = authorizeUser.id
+
+    return question.optionOne.votes.includes(id)
+      || question.optionTwo.votes.includes(id)
+  }
+
   render() {
     const { questions } = this.props
     const questionList = []
     for (let key in questions) {
-      questionList.push(questions[key])
+      const question = questions[key]
+      const voted = this.hasVoted(question)
+      if (this.state.answered) {
+        if (voted) {
+          questionList.push(question)
+        }
+      } else {
+        if (!voted) {
+          questionList.push(question)
+        }
+      }
     }
 
     questionList.sort((a, b) =>
@@ -30,9 +53,10 @@ class Dashboard extends Component {
   }
 }
 
-function mapStateToProps({ questions }) {
+function mapStateToProps({ questions, authorizeUser }) {
   return {
     questions,
+    authorizeUser,
   }
 }
 
